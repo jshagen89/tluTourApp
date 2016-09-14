cbda3f5eb7c0bb553fa54cdad59fe715ace9e997
jshagen <jshagen@tlu.edu>
2016-09-14 15:18:07 -0500

    Completed content provider class

--- app/src/main/java/com/example/joseph/tlucampustour/TourContentProvider.java
+++ app/src/main/java/com/example/joseph/tlucampustour/TourContentProvider.java
@@ -1,9 +1,14 @@
 package com.example.joseph.tlucampustour;
 
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 /**
@@ -11,36 +16,91 @@ import android.support.annotation.Nullable;
  */
 public class TourContentProvider extends ContentProvider{
 
+    private static final String AUTHORITY = "com.example.joseph.tlucampustour.TourContentProvider";
+    public static final String TABLE_TOUR_STOPS = "tourstops";
+    public static final Uri CONTENT_URI = Uri.parse("context//" + AUTHORITY + "/" + TABLE_TOUR_STOPS);
+    public static final int TABLE_REF = 1;
+    public static final int ID_REF = 2;
+
+    private static final UriMatcher myUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        myUriMatcher.addURI(AUTHORITY, TABLE_TOUR_STOPS, TABLE_REF);
+        myUriMatcher.addURI(AUTHORITY, TABLE_TOUR_STOPS, ID_REF);
+    }
+
+    private DBOpenHelper db;
+
     @Override
     public boolean onCreate() {
+        db = new DBOpenHelper(this.getContext(), null, null, 1);
         return false;
     }
 
     @Nullable
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] args, String sortOrder) {
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(TABLE_TOUR_STOPS);
+        int uriType = myUriMatcher.match(uri);
+
+        switch (uriType)
+        {
+            case TABLE_REF:
+                queryBuilder.appendWhere(DBOpenHelper.ID_COL_POSITION + "=" + uri.getLastPathSegment());
+                break;
+            case ID_REF:
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI");
+        }
+        Cursor myCursor = queryBuilder.query(db.getReadableDatabase(), projection, selection, args, null, null, sortOrder);
+
+        if (this.getContext() != null)
+        {
+            myCursor.setNotificationUri(this.getContext().getContentResolver(), uri);
+        }
+
+        return myCursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
         return null;
     }
 
     @Nullable
     @Override
-    public String getType(Uri uri) {
-        return null;
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        int uriType = myUriMatcher.match(uri);
+        SQLiteDatabase sqlDB = db.getWritableDatabase();
+        long id;
+
+        switch (uriType)
+        {
+            case TABLE_REF:
+                id = sqlDB.insert(TABLE_TOUR_STOPS, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI");
+        }
+        if (this.getContext()!= null)
+        {
+            this.getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return Uri.parse(TABLE_TOUR_STOPS + "/" + id);
     }
 
-    @Nullable
+    // Not needed for this application since user cannot delete db records
     @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
-    }
-
-    @Override
-    public int delete(Uri uri, String s, String[] strings) {
+    public int delete(@NonNull Uri uri, String s, String[] strings) {
         return 0;
     }
 
+    // Not needed for this application since user cannot add new db records
     @Override
-    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+    public int update(@NonNull Uri uri, ContentValues contentValues, String s, String[] strings) {
         return 0;
     }
 }
