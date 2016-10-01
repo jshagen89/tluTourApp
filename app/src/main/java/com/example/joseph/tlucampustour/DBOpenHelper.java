2f8cc379634b4187eccb8c4c993dc547c3ccd778
jshagen <jshagen@tlu.edu>
2016-10-01 15:05:44 -0500

    Fixed minor issues with text info for certain Tour Stops
    Added instructions to Tour Stop list

--- app/src/main/java/com/example/joseph/tlucampustour/DBOpenHelper.java
+++ app/src/main/java/com/example/joseph/tlucampustour/DBOpenHelper.java
@@ -28,7 +28,7 @@ class DBOpenHelper extends SQLiteOpenHelper
         myTourStops[0] = new TourStop("Martin Luther Statue", 29.57089, -97.98201, 20, R.string.martin_luther_statue_info, R.drawable.martin_luther_statue, R.raw.martin_luther_statue);
         myTourStops[1] = new TourStop("AT&T & Moody Science Complex", 29.57098, -97.98293, 45, R.string.att_science_info, R.drawable.att_science, R.raw.att_science);
         myTourStops[2] = new TourStop("Emma Frey", 29.57148, -97.98263, 30, R.string.emma_frey_info, R.drawable.emma_frey, R.raw.emma_frey);
-        myTourStops[3] = new TourStop("Tschope Hall", 29.57206, -97.98265, 40, R.string.tschoepe_info, R.drawable.tschoepe, R.raw.tschope);
+        myTourStops[3] = new TourStop("Tschoepe Hall", 29.57206, -97.98265, 40, R.string.tschoepe_info, R.drawable.tschoepe, R.raw.tschope);
         myTourStops[4] = new TourStop("Baldus, Clifton, Trinity Housing", 29.57184, -97.98363, 55, R.string.baldus_clifton_trinity_info, R.drawable.baldus_clifton_trinity_housing, R.raw.baldus_clifton_trinity);
         myTourStops[5] = new TourStop("Krost and Health Sciences", 29.57096, -97.98347, 30, R.string.krost_info, R.drawable.krost_health_sciences, R.raw.krost);
         myTourStops[6] = new TourStop("Weston Ranch", 29, -97, 50, R.string.weston_ranch_info, R.drawable.weston_ranch, R.raw.weston_ranch);
