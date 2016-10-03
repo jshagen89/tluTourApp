bf6705aa5939bc6b5ec0db35b78e359526d99d63
jshagen <jshagen@tlu.edu>
2016-10-03 17:08:46 -0500

    Added Location services to Info Activity to check if user has left radius of current stop

--- app/src/main/java/com/example/joseph/tlucampustour/TourStopInfo.java
+++ app/src/main/java/com/example/joseph/tlucampustour/TourStopInfo.java
@@ -1,26 +1,43 @@
 package com.example.joseph.tlucampustour;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.*;
 
-public class TourStopInfo extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.*;
 
+import static com.example.joseph.tlucampustour.Constants.FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS;
+import static com.example.joseph.tlucampustour.Constants.UPDATE_INTERVAL_IN_MILLISECONDS;
+
+public class TourStopInfo extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private TourStop currStop;
     private AudioPlayer myAudioPlayer;
     private boolean isAudioPlaying;
     private boolean isAudioPaused;
     private ImageButton playPauseButton;
     private AudioManager myAudioManager;
     private SeekBar volumeControl;
+    private GoogleApiClient myGoogleClient;
+    private LocationRequest myLocationRequest;
+    private Location myLocation;
 
 
     private String currName;
@@ -38,7 +55,7 @@ public class TourStopInfo extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        TourStop currStop = getIntent().getExtras().getParcelable("TourStop");
+        currStop = getIntent().getExtras().getParcelable("TourStop");
         if (currStop != null)
         {
             currName = currStop.getName();
@@ -46,18 +63,24 @@ public class TourStopInfo extends AppCompatActivity {
             imgID = currStop.getImage();
             audioID = currStop.getAudioFile();
         }
+        initializeUI();
+        buildGoogleApiClient();
+        initializeAudioControls();
+    }
 
+    private void initializeUI()
+    {
         setTitle(currName);
-
         TextView nameTV = (TextView) findViewById(R.id.tourStopName);
         nameTV.setText(currName);
-
         TextView infoTV = (TextView) findViewById(R.id.tourStopInfo);
         infoTV.setText(infoTextID);
-
         ImageView tourStopIV = (ImageView) findViewById(R.id.tourStopImg);
         tourStopIV.setImageResource(imgID);
+    }
 
+    private void initializeAudioControls()
+    {
         playPauseButton = (ImageButton) findViewById(R.id.playPauseButton);
         volumeControl = (SeekBar) findViewById(R.id.volumeControl);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
@@ -164,6 +187,95 @@ public class TourStopInfo extends AppCompatActivity {
     {
         super.onStop();
         myAudioPlayer.stop();
+    }
+
+    /* ***************************** LOCATION METHODS START HERE ****************************** */
+
+    // Creates the Google API Client with Location Services
+    protected synchronized void buildGoogleApiClient() {
+        myGoogleClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        createLocationRequest();
+
+    }
+
+    protected void createLocationRequest() {
+        myLocationRequest = new LocationRequest();
+        // Sets the desired interval for active location updates
+        myLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        // Sets the fastest rate for active location updates
+        myLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        myLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                myGoogleClient, myLocationRequest, this);
+    }
+
+    // Gets the users current location
+    private Location getCurrentLocation() {
+        try {
+            myLocation = LocationServices.FusedLocationApi.getLastLocation(myGoogleClient);
+            return myLocation;
+        } catch (SecurityException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // Need to set permissions if not done in manifest file by some versions of Android
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        if (myLocation == null)
+        {
+            myLocation = LocationServices.FusedLocationApi.getLastLocation(myGoogleClient);
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        myGoogleClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("Location", "Connection failed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        myLocation = location;
+        float[] distance = new float[2];
+        double myLat = myLocation.getLatitude();
+        double myLon = myLocation.getLongitude();
+        Location.distanceBetween(myLat, myLon, currStop.getLatitude(), currStop.getLongitude(), distance);
+
+        if (distance[0] > currStop.getRadius() && !myAudioPlayer.isPlaying())
+        {
+            finish();
+        }
     }
 
     private class NarrationCompletionListener implements MediaPlayer.OnCompletionListener {
